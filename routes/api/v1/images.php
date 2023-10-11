f1d2ca50695679dcbc9d23f2856ea2c5fb84e582
huyct <huydevct@gmail.com>
2023-10-11 16:25:23 +0700

    detect object route

--- routes/api/v1/images.php
+++ routes/api/v1/images.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\ApiController;
 Route::prefix('images')->group(function (){
 
     Route::post('detect-lp',[ApiController::class,'detectLp'])->name('api.v1.images.detect');
+    Route::post('detect-object',[ApiController::class,'detectObject'])->name('api.v1.images.detect-object');
 
 });
